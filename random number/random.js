b25a33135e4c26141bab7305fba1d9826f34f288
vengeance <preyasguragai43@gmail.com>
2024-12-06 00:24:59 +0545

    modified random number generation of the lud0 adding three more random number generation. no more physical langur purja

--- random number/random.js
+++ random number/random.js
@@ -1,10 +1,22 @@
+const label1=document.getElementById("label1");
+const label2=document.getElementById("label2");
+const label3=document.getElementById("label3");
+const label4=document.getElementById("label4");
 const myButton=document.getElementById("myButton");
-const myLabel=document.getElementById("myLabel");
 const min=1;
 const max=6;
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+let randomNum4;
 
 myButton.onclick=function(){
-    randomNum=Math.floor(Math.random()*max)+min;
-    myLabel.textContent=randomNum;
+    randomNum1=Math.floor(Math.random()*max)+min;
+    randomNum2=Math.floor(Math.random()*max)+min;
+    randomNum3=Math.floor(Math.random()*max)+min;
+    randomNum4=Math.floor(Math.random()*max)+min;
+    label1.textContent=randomNum1;
+    label2.textContent=randomNum2;
+    label3.textContent=randomNum3;
+    label4.textContent=randomNum4;
 }
