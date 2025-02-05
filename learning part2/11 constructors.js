d4670e6c9af873d4bad75043b24fc24c456a659e
vengeance <preyasguragai43@gmail.com>
2025-02-05 19:46:33 +0545

    constructors in js

---
+++ learning part2/11 constructors.js
@@ -0,0 +1,64 @@
+// html file is learning part2.html
+
+//   constructors = special method for defining the properties
+//                   and  methods of objects
+
+/*
+
+function car(MAKE,MODEL,YEAR,COLOR) //constructor created;
+{
+this.make=MAKE,
+this.model=MODEL,
+this.year=YEAR,
+this.color=COLOR
+}
+
+const car1=new car("ford", "mustang", "2024","black"); //object created
+const car2=new car("bugati", "veron", "2022","white-black"); //object created
+
+
+
+
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.color);
+
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
+
+
+
+///////////////////////---------example-2----------------------///////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+function details(firstName,lastName, Age)
+{
+    this.fname=firstName,
+    this.lname=lastName,
+    this.age=Age
+    this.isStudent= function(){console.log("he is a student");}
+    this.isVengeance=()=>{console.log("yeah!!! i am vengeance");}
+}
+
+const std1= new details("ram", "bahadur", 18);
+const std2= new details("hari","bahadur",20);
+const std3= new details("call me", "vengeance", 1000);
+
+console.log(std1.fname);
+console.log(std1.lname);
+console.log(std1.age);
+std1.isStudent();
+
+console.log(std2.fname);
+console.log(std2.lname);
+console.log(std2.age);
+
+console.log(std3.fname);
+console.log(std3.lname);
+console.log(std3.age);
+std3.isVengeance();
+
+*/
